0036341229ff2c1cf961dcaaba43d0b4ba3170a1
Antonio Marqués <amarques@aidacanarias.com>
2024-09-22 13:18:47 +0200

    feat: added distance calculator

--- csharp/dotnet/Kata/Manhattan.Test/ManhattanDistanceTests.cs
+++ csharp/dotnet/Kata/Manhattan.Test/ManhattanDistanceTests.cs
@@ -1,5 +1,5 @@
 using FluentAssertions;
-using System.Drawing;
+using ManhattanDistanceCalculator;
 
 namespace Manhattan.Test
 {
@@ -8,19 +8,37 @@ namespace Manhattan.Test
         [SetUp]
         public void Setup()
         {
+
         }
 
         [Test]
         public void same_point_distance_should_return_zero()
         {
             var point = new Point(1, 1);
-            var distance = manhattanDistance(point, point);
+            var distance = DistanceCalculator.manhattanDistance(point, point);
             distance.Should().Be(0);
         }
 
-        private object manhattanDistance(object point, object o)
+        [Test]
+
+        public void point_distance_should_return_four()
         {
-            throw new NotImplementedException();
+            var point1 = new Point(5, 4);
+            var point2 = new Point(3, 2);
+            var distance = DistanceCalculator.manhattanDistance(point1, point2);
+            distance.Should().Be(4);
         }
+
+        [Test]
+
+        public void point_distance_should_return_three()
+        {
+            var point1 = new Point(1, 1);
+            var point2 = new Point(0, 3);
+            var distance = DistanceCalculator.manhattanDistance(point1, point2);
+            distance.Should().Be(3);
+        }
+
+
     }
 }
---
+++ csharp/dotnet/Kata/ManhattanDistanceCalculator/DistanceCalculator.cs
@@ -0,0 +1,14 @@
+﻿namespace ManhattanDistanceCalculator
+{
+    public class DistanceCalculator
+    {
+        public static int manhattanDistance(Point p1, Point p2)
+        {
+
+            var abs_x = Math.Abs(p1.GetX() - p2.GetX());
+            var abs_y = Math.Abs(p1.GetY() - p2.GetY());
+
+            return abs_x + abs_y;
+        }
+    }
+}
